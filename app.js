e018ab875b61a691894769ae023f7b2689e21a80
Harshit Gupta <guptaharshit901@gmail.com>
2023-02-10 16:55:16 +0530

    updated CSS
    added some new css features

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 console.log(txtInput);
 
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverUrl="https://api.funtranslations.com/translate/minion.json";
+
+
+
+function getTranslationUrl(text){
+    return serverUrl + "?" + "text= "+ text;
+}
+
+
 function clickHandler(){
-    console.log("clicked!");
-    console.log("input",txtInput.value);
+
+
+  var inputTxt = txtInput.value ; // taking input
+
+  // calling server for processing
+  fetch(getTranslationUrl(inputTxt))
+    .then( response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;  // output
+    })
+  
 }
 
 btnTranslate.addEventListener("click",clickHandler);
